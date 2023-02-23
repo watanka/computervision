35a6ae3c24926b043d7a26504b267a5f8c7e75c3
eunsung <eunsung.shin@gmail.com>
2023-02-23 22:54:45 +0900

    perspective transform

---
+++ perspective_transform.py
@@ -0,0 +1,37 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# 해당 이미지에서 모니터 이미지 정보를 읽어오고 싶다. opencv 내장함수 없이 perspective transform을 구현해보자.
+
+# read image
+img = cv2.imread('img/get_monitor_perspective.jpg')
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+# 모니터의 네 꼭짓점을 수기로 기록한다.
+def click_event(event, x,y, flags, params) :
+    if event == cv2.EVENT_LBUTTONDOWN :
+        print(x, y)
+        cv2.putText(img, f'{(x,y)}', (x,y), font, 1, (255, 0, 0), 2)
+        cv2.circle(img, (x,y), 2, (0,0,255), 3)
+        cv2.imshow('image', img)
+
+cv2.imshow('image', img)
+cv2.setMouseCallback('image', click_event)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# 네 꼭짓점 (y,x)
+top_left = (203, 279)
+top_right = (431, 868)
+bottom_right = (802, 864)
+bottom_left = (855, 274)
+
+pts = [top_left, top_right, bottom_right, bottom_left]
+# 이 꼭짓점에 포함되는 이미지 잘라서 확인하기
+
+crop_img = img[min([p[0] for p in pts]) : max([p[0] for p in pts]), min([p[1] for p in pts]) : max([p[1] for p in pts])] 
+
+cv2.imshow('crop image', crop_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
